a25962512d7bbf989becbae70f64b4c0f95e46cf
Lance Zotigh <lancezotighlz@gmail.com>
2019-01-24 16:36:06 -0700

    changed the format and deleted the semi colon

--- src/edu/cnm/deepdive/numberGuesser.java
+++ src/edu/cnm/deepdive/numberGuesser.java
@@ -9,33 +9,39 @@ public class numberGuesser {
 
   public static void main(String[] args) {
 
-    boolean playAgain;
+    while ( playOneRound()){
 
+    }
+
+  }
+
+  private static boolean playOneRound() {
+    int secret = random.nextInt(100) + 1;
+
+    while (guessAndCheck(secret));
+    System.out.println("play again?");
+    String reaction;
     do {
-      int secret = random.nextInt(100) + 1;
-      boolean correct = false;
-      boolean forfeit = false;
-      do {
-        System.out.println("Guess a number between 1-100");
-        int guess = input.nextInt();
-        if (guess == 0) {
-          forfeit = true;
-          System.out.println("Pathetic!");
-        } else if (guess == secret) {
-          correct = true;
-          System.out.println("correct");
-        } else if (guess < secret) {
-          System.out.println("guess is too low.");
-        } else {
-          System.out.println("guess is too high.");
-        }
-      } while (!correct && !forfeit);
-      System.out.println("play again?");
-      String reaction;
-      do {
-        reaction = input.nextLine().trim().toLowerCase();
-      } while (reaction.isEmpty());
-      playAgain = reaction.charAt(0) == 'y';
-    } while (playAgain);
+      reaction = input.nextLine().trim().toLowerCase();
+    } while (reaction.isEmpty());
+    return reaction.charAt(0) == 'y';
+  }
+
+  private static boolean guessAndCheck(int secret) {
+    boolean guessAgain = true;
+    System.out.println("Guess a number between 1-100");
+    int guess = input.nextInt();
+    if (guess == 0) {
+      guessAgain = false;
+      System.out.println("Pathetic!");
+    } else if (guess == secret) {
+      guessAgain = false;
+      System.out.println("correct");
+    } else if (guess < secret) {
+      System.out.println("guess is too low.");
+    } else {
+      System.out.println("guess is too high.");
+    }
+    return guessAgain;
   }
 }
